bfe307e938c2ed7505342de328e3c12a7d95e260
Joerg Henning <henning.joerg@gmail.com>
2015-06-01 12:57:55 +0800

    Store params in redis

--- sinatra-web/webapp/lib/app.rb
+++ sinatra-web/webapp/lib/app.rb
@@ -1,16 +1,27 @@
 require "rubygems"
 require "sinatra"
 require "json"
+require "uri"
+require "redis"
 
 class App < Sinatra::Application
+
+  uri = URI.parse(ENV["DB_PORT"])
+  redis = Redis.new(:host => uri.host, :port => uri.port)
 
   set :bind, '0.0.0.0'
 
   get '/' do
     "<h1>DockerBook Test Sinatra app</h1>"
   end
+  
+  get '/json' do 
+    params = redis.get "params"
+    params.to_json
+  end
 
   post '/json/?' do
+    redis.set "params", [params].to_json
     params.to_json
   end
 
